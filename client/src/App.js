ef5c8c05156fd253d063fe1c20bdb74ebaf27aea
Rittik Basak Utsha <rittik.basak@gmail.com>
2022-07-18 04:00:20 +0600

    Fixed Merge issues

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,12 @@ import React from 'react'
 import "bootstrap/dist/css/bootstrap.min.css";
 
 // We use Route in order to define the different routes
-import { Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 // We import all the components we need in our app
 import Home from "./components/Home"
 import Register from "./components/Register"
-import Login from "./components/Login"
+import Login from "./components/login"
 import UserList from "./components/UserList"
 import Navbar from "./components/navbar";
 import AdminHome from "./components/adminHome"
@@ -18,20 +18,23 @@ import BookProfile from "./components/book-profile";
 
 const App = () => {
 	return (
-		<div>
-			<Routes>
-				<Route path="/" exact element={<Home />} />
-				{/* <Route exact path="/" element={< Login/> }/> */}
-				{/* <Route path="/admin" element={< AdminHome/> }/> */}
-				<Route path="/register" exact element={<Register />} />
-				<Route path="/login" exact element={<Login />} />
-				<Route path="/userlist" exact element={<UserList />} />
-				<Route path="/books" element={< BookList/> }/>
-				<Route path="/books/:id" element={< BookProfile/> }/>
-				<Route path="/books/edit/:id" element={< EditBook/> }/>
-				<Route path="/admin/addbook" element={< AddBook/> }/>
-			</Routes>
-		</div>
+    <Router>
+      <Navbar />
+      <div>
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          {/* <Route exact path="/" element={< Login/> }/> */}
+          {/* <Route path="/admin" element={< AdminHome/> }/> */}
+          <Route path="/register" exact element={<Register />} />
+          <Route path="/login" exact element={<Login />} />
+          <Route path="/userlist" exact element={<UserList />} />
+          <Route path="/books" element={< BookList/> }/>
+          <Route path="/books/:id" element={< BookProfile/> }/>
+          <Route path="/books/edit/:id" element={< EditBook/> }/>
+          <Route path="/admin/addbook" element={< AddBook/> }/>
+        </Routes>
+      </div>
+    </Router>
 	);
 };
 
