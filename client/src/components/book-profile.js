78f02483460e76070f72c666b2b9a8f024e6c352
SuperSharjil <sharjilmahir@gmail.com>
2022-08-28 14:14:16 +0600

    merged with Rittik

--- client/src/components/book-profile.js
+++ client/src/components/book-profile.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { MouseEvent } from 'react';
+import { useState } from 'react';
+import { Link, Navigate } from 'react-router-dom';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import axios from 'axios';
+import Container from 'react-bootstrap/esm/Container';
+import Checkbox from '@mui/material/Checkbox';
+import Avatar from '@mui/material/Avatar';
+import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder';
+import BookmarkIcon from '@mui/icons-material/Bookmark';
+import Typography from '@mui/material/Typography';
+import Rating from '@mui/material/Rating';
+import { Grid } from '@mui/material';
+
+const NavbarHeight = '60px';
+/*
+const [anchorEl, setAnchorEl] = useState<HTMLButtonElement | null>(null);
+const open = Boolean(anchorEl);
+const id = open ? 'simple-popover' : undefined;
+*/
+
+const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
 
 export default class Book extends Component{
   constructor(props){
@@ -65,6 +84,24 @@ export default class Book extends Component{
       console.log(error);
     });
   }
+
+
+
+  /*
+//rating popover
+
+
+  handleClick(e) {
+    setAnchorEl(e.currentTarget);
+  };
+
+  handleClose(){
+    setAnchorEl(null);
+  };
+
+
+*/
+
 
 
   calc_rating(){
@@ -132,39 +169,24 @@ export default class Book extends Component{
     );
   }
 
-  setReadItem = async () => {
-    const response = await fetch('http://localhost:5000/read/setReadItem/'+this.state.id, {
-      method: 'GET',
-      headers: {
-        'token': localStorage.getItem('token'),
-      },
-    });
-
-    console.log(response);
-
-    //go to read page
-    window.location.href = '/books/'+this.state.book._id+'/read';
-  }
-
   renderEditButton(){
     if (this.state.user.role === "Admin") {
       return (
-        <div>
-          <Link to={'/books/edit/'+this.state.book._id}>
-            Edit
+        <Container style={{display:"flex", alignItems:"center", justifyContent:"center"}}>
+          <Link className='btn btn-danger' to={'/books/edit/'+this.state.book._id}>
+            Edit Book
           </Link>
-      </div>
+        </Container>
       );
     }
   }
 
-
   renderMyListButton(){
     if(this.state.user.role === "Basic" && this.state.user.subscription){
       return (
-        <div>
+        <Container>
           <Button className="float-end" size="sm" variant="info" onClick={this.addToMyList}>Add to My List</Button>
-        </div>
+        </Container>
       );
     }
   }
@@ -173,18 +195,29 @@ export default class Book extends Component{
   {
     if(this.state.user.role === "Basic" && this.state.user.subscription){
       return (
-        <div>
-          <Button className="float-end" size="sm" variant="info" onClick={this.setReadItem}>Read</Button>
-          {/* <Link to={'/books/'+this.state.book._id+'/read'}>Read</Link> */}
-        </div>
+        <Container style={{display:"flex", alignItems:"center", justifyContent:"center"}}>
+          <Button variant='btn btn-danger' onClick={this.setReadItem}>Read</Button>
+        </Container>
       );
     }
   }
 
+  setReadItem = async() =>{
+    const response = await fetch('http://localhost:5000/read/setReadItem/'+this.state.id, {
+      method: 'GET',
+      headers: {
+        'token': localStorage.getItem('token'),
+      }, 
+    });
+
+    console.log(response);
+
+    window.location.href = '/books/' + this.state.book._id+ '/read' ;
+  }
 
   renderReviews(){
       return (
-        <div>
+        <Container>
           <h3>Reviews</h3>
           {this.state.reviews.map((review) => {
             return (
@@ -197,7 +230,7 @@ export default class Book extends Component{
             );
           }
           )}
-        </div>
+        </Container>
       );
   }
 
@@ -205,73 +238,95 @@ export default class Book extends Component{
   renderReviewBox(){
     if (this.state.user.role === "Basic" && this.state.user.subscription) {
       return (
-        <div>
-          <Form.Label column="sm" lg={2}>Rating:</Form.Label>
-          <Form.Control className="w-100" size="sm" as="select" onChange={this.onChangeRating} value={this.state.rating}>
-            <option>0</option>
-            <option>1</option>
-            <option>2</option>
-            <option>3</option>
-            <option>4</option>
-            <option>5</option>
-          </Form.Control>
-
-          <Form.Label column="sm" lg={2}>Write review:</Form.Label>
+        <Container style={{width:"50vw"}}fixed>
+          <Rating name="simple-controlled" size="large" value={this.state.rating} onChange={this.onChangeRating} />
           <Form.Control className="w-100" size="sm" as="textarea" onChange={this.onChangeDescription} value={this.state.description} placeholder="" />
-          
-          <Button className="float-end" size="sm" variant="info" onClick={this.addReview}>Save</Button>
-        </div>
+          <Button className="float-end" size="sm" variant="warning" style={{width:"150px", color:"brown"}} onClick={this.addReview}>Save</Button>
+          <br/><br/>
+        </Container>
       );
     }
   }
 
   render(){
     return (
-      <div>
-        <Row>
-          <Col></Col>
-          <Col>
-            <img src= {"http://localhost:5000" + this.state.book.coverLocation} alt="" height='350px' width='250px'/>
-            { this.renderReadButton() }
-            { this.renderEditButton() }
-          </Col>
-          <Col>
-            <h1>{this.state.book.title} </h1>
-            <h3> {this.state.avg_rating.toFixed(1)}/5</h3>
-            <h3>{this.state.book.author}</h3>
-            <h5>{this.state.genre}</h5>
-            <p>{this.state.book.description}</p>
-          </Col>
-          <Col>
-            {this.renderMyListButton()}
-          </Col>
-          <Col></Col>
-        </Row>
-        <Row>
-          <Col></Col>
-          <Col>
-          <div>
-            <h3>Reviews</h3>
+      <Container style={{display: "flex", flexDirection:"row", backgroundColor:"#fff0cc", height:'calc(100vh - 70px)'}} fluid>
+        <Container style={{flex:2 , display:"flex", flexDirection:"column", alignItems: "center", justifyContent:"center", backgroundColor:"#ffe3a1", margin:"20px"}} fluid>
+          <img src= {"http://localhost:5000" + this.state.book.coverLocation} alt="" height='85%' margin='20px'/>
+          <br />
+          { this.renderReadButton() }
+          { this.renderEditButton() }
+        </Container>
+
+        <Container style={{flex:3 , display:"flex", flexDirection:"column", marginTop:20, marginBottom:20}} fluid>
+          <Container style={{backgroundColor:"#ffe3a1", height:"15vh", borderRadius:5, margin:5, padding:20}} fluid>
+            <Grid container spacing={0}>
+              <Grid xs={11}>
+                <Typography style={{fontSize:36, fontFamily:'Roboto'}}>{this.state.book.title}</Typography>
+                <Typography style={{fontSize:16, color:"brown", margin:5}}><b>{this.state.book.author}</b></Typography>
+              </Grid>
+              <Grid xs={1}>
+                <Checkbox className="float-end" {...label} onClick={this.addToMyList} icon={<BookmarkBorderIcon />} checkedIcon={<BookmarkIcon />} />
+              </Grid>
+            </Grid>
+          </Container>
+
+
+          <Container style={{backgroundColor:"#dedede", borderRadius:5, margin:5, padding:5, paddingLeft:20}} fluid>
+            <Rating name="read-only" value={this.state.avg_rating} precision={0.1} size="large" readOnly />
+          </Container>
+
+
+          <Container style={{height:"30vh", overflow:"scroll", backgroundColor:"#ffe3a1", borderRadius:5, margin:5, padding:20}} fixed>
+            <Typography style={{fontSize:14, fontFamily:'Roboto'}}>{this.state.book.description}</Typography>
+          </Container>
+
+
+          <Container style={{display:"flex", flexDirection:"row", backgroundColor:"#dedede", borderRadius:5, margin:5, padding:5, paddingLeft:20}} fluid>
+            <Typography style={{color:"white", backgroundColor:"grey", borderRadius:20, margin:5, padding:5, paddingLeft:10, paddingRight:10}}>
+              Category
+            </Typography>
+
+
+            <Typography style={{color:"white", backgroundColor:"orange", borderRadius:20, margin:5, padding:5, paddingLeft:10, paddingRight:10}}>
+              {this.state.genre}
+            </Typography>
+
+
+          </Container>
+
+
+          <Container style={{height:"50vh", overflow:"scroll", backgroundColor:"#ffe3a1", borderRadius:5, margin:5, padding:20}} fixed>
+            { this.renderReviewBox() }
+
             {this.state.reviews.map((review) => {
               return (
-                <Row>
-                  <Col>
-                    <h5>User: {review.userID.username}</h5>
-                    <h5>Rating: {review.rating}</h5>
-                    <h5>Description: {review.description}</h5>
-                  </Col>
-                </Row>
+                <Container style={{width:"100%", backgroundColor:"#d9ffbf", borderRadius:5, margin:10, marginLeft:"auto", marginRight:"auto", padding:20}} fluid>
+                    <Row>
+                      <Col xs={1}>
+                        <Avatar alt={review.userID.username} src="http://localhost:5000/images/1.png"  style={{margin:5}}/>
+                      </Col>
+                      <Col xs={11}>
+                      <Rating name="read-only" value={review.rating} precision={1} size="small" readOnly />
+                      <br />
+                      <b style={{fontSize:18}} >
+                        {review.userID.username}
+                      </b>
+                      <br />
+                      <i class="text-break" style={{fontSize:14}}>
+                        {review.description}
+                      </i>
+                      </Col>
+                    </Row>
+                </Container>
               );
             }
             )} 
-          </div>
-          <div >
-            { this.renderReviewBox() }
-          </div>
-          </Col>
-          <Col></Col>
-        </Row>
-      </div>
+
+          </Container>
+        </Container>
+      </Container>
+
     );
   }
 }
